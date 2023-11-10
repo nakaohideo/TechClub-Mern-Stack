9327c8571b9dfa26d3463f033d4db36ad03c43e2
nakaohideo <ninjanakao123@gmail.com>
2023-11-10 11:17:28 +0300

    CTC-webite-Mern-Stack-Login-Commit

--- frontend/src/components/Admin/login.jsx
+++ frontend/src/components/Admin/login.jsx
@@ -1,68 +1,84 @@
 import React, { useState } from "react";
-import { FaUser, FaLock } from "react-icons/fa"; // Importing icons from react-icons
+import { Link } from "react-router-dom";
+import { FaUser, FaLock } from "react-icons/fa";
 import { correctUsername, correctPassword } from "./credentials.js";
-import CreatePosts from "./CreatePost.jsx";
 
-const LoginPage = () => {
+const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [loginSuccess, setLoginSuccess] = useState(null);
 
   const handleLogin = () => {
-    // Check if the entered username and password are correct
-    if (username === correctUsername && password === correctPassword) {
-      // Redirect to another page on successful login
-      window.location.href = "admin";
+    // Check if the username and password are correct
+    const isValidCredentials =
+      username === correctUsername && password === correctPassword;
+
+    if (isValidCredentials) {
+      // Set login success state to true
+      setLoginSuccess(true);
     } else {
-      // Display an error message for incorrect credentials
-      setErrorMessage("Username and password are incorrect. Please try again.");
+      // Set login success state to false
+      setLoginSuccess(false);
     }
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="bg-white p-8 rounded shadow-md w-96">
-        <h1 className="text-2xl font-bold mb-4">Login Page</h1>
+    <div className="h-screen">
+      <div className="max-w-md mx-auto mt-20 p-6 bg-white border rounded shadow-md">
+        <h2 className="text-2xl mb-4">Login</h2>
+
         <div className="mb-4">
-          <label className="block text-gray-600">Username:</label>
-          <div className="relative">
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <span className="absolute right-3 top-2">
-              <FaUser />
-            </span>
-          </div>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            <FaUser className="inline-block mr-2" />
+            Username:
+          </label>
+          <input
+            type="text"
+            className="w-full border rounded py-2 px-3"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
+
         <div className="mb-4">
-          <label className="block text-gray-600">Password:</label>
-          <div className="relative">
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <span className="absolute right-3 top-2">
-              <FaLock />
-            </span>
-          </div>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            <FaLock className="inline-block mr-2" />
+            Password:
+          </label>
+          <input
+            type="password"
+            className="w-full border rounded py-2 px-3"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <button
-          onClick={handleLogin}
-          className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
-        >
-          Login
-        </button>
-        {errorMessage && (
-          <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
-        )}
+
+        <div className="flex justify-around items-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+
+          {loginSuccess && (
+            <div className="mt-4">
+              <p className="text-green-600">Login successful!</p>
+              <Link to="/admin" className="text-blue-500 hover:underline">
+                Go to Admin Dashboard
+              </Link>
+            </div>
+          )}
+
+          {!loginSuccess && loginSuccess !== null && (
+            <p className="text-red-600 mt-4">
+              Username and password are incorrect. Please try again.
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
 };
 
-export default LoginPage;
+export default LoginForm;
